495c5662ae8b6e93905aa9857f5a987a8426ede0
xxKeefer <xxkeefer@pm.me>
2026-01-06 18:25:09 +1000

    refactor: :coffin: remove dead code

--- src/components/team-layout/index.ts
+++
@@ -1,8 +0,0 @@
-import TeamLayout from './TeamLayout.vue'
-import MainRow from './MainRow.vue'
-import BackUpRow from './BackUpRow.vue'
-
-export default Object.assign(TeamLayout, {
-  MainRow,
-  BackUpRow,
-})
